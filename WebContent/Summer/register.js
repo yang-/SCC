9198d7e4d5bf6ed25bb81b1bfaecba5466de788a
Yang Chen <cxfy123@gmail.com>
2013-04-27 17:31:40 -0400

    New version of register page in /WebContent/Summer/

--- WebContent/Summer/register.js
+++ WebContent/Summer/register.js
@@ -1,7 +1,19 @@
+$(function(){
+	$(".offCampus").prop("disabled", true);
+	$(".offCampus:input").prop("required", false);
+	$( document ).tooltip({ position: { my: "left+15 center", at: "right center" } });
+	var today = new Date();
+	var startDate = new Date(2013, 7, 1);
+	startDate = today.getTime()<startDate.getTime()?startDate:today;
+	$( "#form-flightDate" ).datepicker({ minDate: startDate, maxDate: new Date(2013, 8, 1) });
+});
+
 function formDestinationChange(select){
 	if (select == "other") {
-		$("#offCampus").css("display", "inline");
+		$(".offCampus").prop("disabled", false);
+		$(".offCampus:input").prop("required", true);
 	} else {
-		$("#offCampus").css("display", "none");
+		$(".offCampus").prop("disabled", true);
+		$(".offCampus:input").prop("required", false);
 	};			
 }
