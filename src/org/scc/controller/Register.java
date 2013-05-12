ab74f216169e85320993ae89bccfe237e5dfb677
Yang Chen <cxfy123@gmail.com>
2013-05-12 00:19:38 -0400

    Fix merge problem

--- src/org/scc/controller/Register.java
+++ src/org/scc/controller/Register.java
@@ -22,11 +22,11 @@ import org.scc.model.AirlineInfo;
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String SUCCESS = "/done.html";
-    private static String FAIL = "/error.html";
+	private static String FAIL = "/error.html";
 
 	public Register() {
 		super();
-//		dao = new UserDao();
+		// dao = new UserDao();
 	}
 
 	/**
@@ -46,8 +46,8 @@ public class Register extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 
 		UserDao dao = new UserDao();
-		String forward="";
-		
+		String forward = "";
+
 		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
@@ -77,22 +77,22 @@ public class Register extends HttpServlet {
 				.getParameter("flightArrivalTerminal"));
 		user.setTerminal(terminal);
 
-//		user.setFirstName(request.getParameter("firstName"));
-//		user.setLastName(request.getParameter("lastName"));
-		
+		// user.setFirstName(request.getParameter("firstName"));
+		// user.setLastName(request.getParameter("lastName"));
+
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
-		firstName = new String(firstName.getBytes("ISO-8859-1"),"UTF-8");
-		lastName = new String(lastName.getBytes("ISO-8859-1"),"UTF-8");
+		firstName = new String(firstName.getBytes("ISO-8859-1"), "UTF-8");
+		lastName = new String(lastName.getBytes("ISO-8859-1"), "UTF-8");
 		user.setFirstName(firstName);
 		user.setLastName(lastName);
-		
+
 		user.setInitDate(new Date());
 		user.setEmail(request.getParameter("email"));
 		user.setPhone(request.getParameter("emergencyContact"));
 		user.setQQ(request.getParameter("qq"));
 		String qqName = request.getParameter("qqName");
-		qqName = new String(qqName.getBytes("ISO-8859-1"),"UTF-8");
+		qqName = new String(qqName.getBytes("ISO-8859-1"), "UTF-8");
 		user.setQQName(qqName);
 		int sbuid = Integer.parseInt(request.getParameter("studentId"));
 		user.setSbuId(sbuid);
@@ -100,14 +100,18 @@ public class Register extends HttpServlet {
 
 		String des = request.getParameter("destination");
 		user.setDestination(des);
-		if (des.equals("other"))
-			user.setOffCampusRow(request.getParameter("offCampus"));
-		else
+		if (des.equals("other")) {
+			String offCampus = request.getParameter("offCampus-addr1")
+					+ request.getParameter("offCampus-addr2")
+					+ request.getParameter("offCampus-city")
+					+ request.getParameter("offCampus-zip");
+			user.setOffCampusRow(offCampus);
+		} else
 			user.setOffCampusRow(null);
 
 		String memo = request.getParameter("memo");
-		if(memo != null){
-			memo = new String(memo.getBytes("ISO-8859-1"),"UTF-8");
+		if (memo != null) {
+			memo = new String(memo.getBytes("ISO-8859-1"), "UTF-8");
 			user.setMemo(memo);
 		} else {
 			user.setMemo(null);
@@ -119,11 +123,11 @@ public class Register extends HttpServlet {
 			forward = FAIL;
 		}
 		dao.closeConnection();
-		
+
 		RequestDispatcher view = request.getRequestDispatcher(forward);
-        view.forward(request, response);
-//		response.sendRedirect(forward);
-//        System.out.println(lastName+firstName);
+		view.forward(request, response);
+		// response.sendRedirect(forward);
+		// System.out.println(lastName+firstName);
 	}
 
 }
