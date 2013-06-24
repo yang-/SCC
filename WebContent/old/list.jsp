e2c44d7623242cebb2fa52a2d77713c171dda875
Yang Chen <cxfy123@gmail.com>
2013-06-24 00:00:26 -0400

    Move to the new design. Use ajax to do the form submission.

---
+++ WebContent/old/list.jsp
@@ -0,0 +1,81 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="description"/>
+<meta name="keywords" content="keywords"/> 
+<meta name="author" content="author"/> 
+<link rel="stylesheet" type="text/css" href="default.css" media="screen"/>
+<title>施福基督教会 Suffolk Christian Church | 校园团契 Campus Fellowship</title>
+</head>
+<body>
+
+<div class="header">
+	<h1>施福基督教会 Suffolk Christian Church</h1>
+	<h2>校园团契 Campus Fellowship</h2>
+	<h3>2013年新生接机</h3>
+</div>
+
+<div class="navigation">
+	<a href="index.html">Home</a>
+	<a href="register.html">Register for Airport Pick-up 2013</a>
+	<a href="contact-us.html">Contact Us</a>
+	<div class="clearer"><span></span></div>
+</div>
+
+<div class="container">
+	<div class="content">
+		<table border=1>
+	        <thead>
+	            <tr>
+	                <th>Name</th>
+	                <th>SBU Id</th>
+	                <th>Flight Date</th>
+	                <th>Arrival Time</th>
+	                <th>Flight Number</th>	                
+	                <th>Arrival Terminal</th>
+	                <th>Apartment</th>
+	                <th>Off Campus Address</th>
+	                <th>QQ</th>
+	                <th>QQ Name</th>
+	                <th>Email</th>
+	                <th>Emergency Phone</th>
+	                <th colspan=2>Action</th>
+	            </tr>
+	        </thead>
+	        <tbody>
+	            <c:forEach items="${users}" var="user">
+	                <tr>
+	                    <td><c:out value="${user.lastName}${user.firstName}" /></td>
+	                    <td><c:out value="${user.userId}" /></td>
+	                    <td><fmt:formatDate pattern="MM/dd/yyyy" value="${user.arrivalDate}" /></td>
+	                    <td><fmt:formatTime pattern="hh:mm" value="${user.arrivalTime}" /></td>
+	                    <td><c:out value="${user.fightNumber}" /></td>
+	                    <td><c:out value="${user.terminal}" /></td>
+	                    <td><c:out value="${user.destination}" /></td>
+	                    <td><c:out value="${user.offCampusRow}" /></td>
+	                    <td><c:out value="${user.QQ}" /></td>
+	                    <td><c:out value="${user.QQName}" /></td>
+	                    <td><c:out value="${user.email}" /></td>
+	                    <td><c:out value="${user.phone}" /></td>
+	                    <td><a href="UserController?action=edit&userId=<c:out value="${user.userid}"/>" >Update</a></td>
+	                    <td><a href="UserController?action=delete&userId=<c:out value="${user.userid}"/>" >Delete</a></td>
+	                </tr>
+	            </c:forEach>
+	        </tbody>
+	    </table>
+	    <p><a href="UserController?action=insert">Add User</a></p>
+	</div>
+
+	<div class="footer">
+		&copy; 2013 <a href="index.html">Suffolk Christian Church Campus Fellowship</a>. All rights reserved.
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ src/org/scc/controller/RegisterWS.java
@@ -0,0 +1,136 @@
+package org.scc.controller;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.scc.dao.UserDao;
+import org.scc.model.AirlineInfo;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/RegisterWS")
+public class RegisterWS extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public RegisterWS() {
+		super();
+		// dao = new UserDao();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("+1");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		UserDao dao = new UserDao();
+
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+		AirlineInfo user = new AirlineInfo();
+		// 基本信息
+		// lastName
+		String lastName = request.getParameter("lastName");
+		lastName = new String(lastName.getBytes("ISO-8859-1"), "UTF-8");
+		user.setLastName(lastName);
+		// firstName
+		String firstName = request.getParameter("firstName");
+		firstName = new String(firstName.getBytes("ISO-8859-1"), "UTF-8");
+		user.setFirstName(firstName);
+		// email
+		user.setInitDate(new Date());
+		user.setEmail(request.getParameter("email"));
+		// emergencyContact
+		String emergencyContact = request.getParameter("emergencyContact");
+		emergencyContact = new String(emergencyContact.getBytes("ISO-8859-1"), "UTF-8");
+		user.setPhone(emergencyContact);
+		// 航班信息
+		// flightArrivalDate
+		String arrivalDateStr = request.getParameter("flightArrivalDate");
+		Date arrivalDate;
+		try {
+			arrivalDate = dateFormat.parse(arrivalDateStr);
+			user.setArrivalDate(new java.sql.Date(arrivalDate.getTime()));
+		} catch (ParseException e) {
+			System.out.println(e);
+		}
+		// flightArrivalTime
+		String arrivalTimeStr = request.getParameter("flightArrivalTime");
+		Date arrivalTime;
+		try {
+			arrivalTime = timeFormat.parse(arrivalTimeStr);
+			user.setArrivalTime(new java.sql.Time(arrivalTime.getTime()));
+		} catch (ParseException e) {
+			System.out.println(e);
+		}
+		// flightNumber
+		user.setFlightNumber(request.getParameter("flightNumber"));
+		// flightArrivalTerminal
+		int terminal = Integer.parseInt(request
+				.getParameter("flightArrivalTerminal"));
+		user.setTerminal(terminal);
+		// 石溪信息
+		// destination
+		String des = request.getParameter("destination");
+		user.setDestination(des);
+		// 校外地址
+		if (des.equals("other")) {
+			String offCampus = request.getParameter("offCampus-addr1") + ", "
+					+ request.getParameter("offCampus-addr2") + ", "
+					+ request.getParameter("offCampus-city") + ", "
+					+ request.getParameter("offCampus-state") + ", "
+					+ request.getParameter("offCampus-zip");
+			user.setOffCampusRow(offCampus);
+		} else {
+			user.setOffCampusRow(null);
+		}
+		// qqName
+		String qqName = request.getParameter("qqName");
+		qqName = new String(qqName.getBytes("ISO-8859-1"), "UTF-8");
+		user.setQQName(qqName);
+		// qq#
+		user.setQQ(request.getParameter("qq"));
+		// major
+		user.setMajorId(request.getParameter("major"));
+		// studentId
+		int sbuid = Integer.parseInt(request.getParameter("studentId"));
+		user.setSbuId(sbuid);
+		// memo
+		String memo = request.getParameter("memo");
+		memo = new String(memo.getBytes("ISO-8859-1"), "UTF-8");
+		user.setMemo(memo);
+		
+		// dao
+		if (dao.addUser(user)) {
+			response.getWriter().write("good");
+		} else {
+			response.getWriter().write("bad");
+		}
+//		System.out.println(lastName);
+	}
+
+}
